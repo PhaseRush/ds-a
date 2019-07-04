8e7403fba0cba1a6ef9004f7934deb7d88dd8d6a
PhaseRush <leozhang888@yahoo.ca>
2019-07-04 12:32:35 -0700

    added heaps

---
+++ src/main/datastructures/trees/heaps/Heap.java
@@ -0,0 +1,75 @@
+package datastructures.trees.heaps;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public abstract class Heap<T> {
+  private T[] array;
+  private int size;
+  private int maxSize;
+  private Comparator<T> comparator;
+
+  public static final int ROOT = 1; // 1 indexed array
+
+  @SuppressWarnings("unchecked")
+  public Heap(int maxSize, Comparator<T> comparator) {
+    this.maxSize = maxSize;
+    this.comparator = comparator;
+    array = (T[]) new Object[maxSize];
+  }
+
+  public boolean insert(T t) {
+    if (size >= maxSize) return false;
+    array[size] = t;
+    size++;
+
+    int temp = size;
+
+    while (greaterThan(array[parent(temp)], array[temp])) {
+      swap(temp, parent(temp));
+      temp = parent(temp);
+    }
+    return true;
+  }
+
+  public T remove() {
+    T root = array[ROOT];
+    array[ROOT] = array[size--];
+    heapify(ROOT);
+    return root;
+  }
+
+  abstract void heapify(int curr);
+
+  public void genHeap() {
+    for (int i = size / 2; i >= ROOT; i--) {
+      heapify(i);
+    }
+  }
+
+  boolean greaterThan(T a, T b) {
+    return Objects.compare(a, b, comparator) > 0;
+  }
+
+  int parent(int curr) {
+    return curr / 2;
+  }
+
+  int leftChild(int curr) {
+    return curr * 2;
+  }
+
+  int rightChild(int curr) {
+    return curr * 2 + 1;
+  }
+
+  boolean isLeaf(int curr) {
+    return curr >= size / 2 && curr <= size;
+  }
+
+  void swap(int i, int j) {
+    T temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
---
+++ src/main/datastructures/trees/heaps/MinHeap.java
@@ -0,0 +1,29 @@
+package datastructures.trees.heaps;
+
+import java.util.Comparator;
+
+public class MinHeap<T> extends Heap<T> {
+  private T[] array;
+  private int size;
+  private int maxSize;
+  private Comparator<T> comparator;
+
+  public MinHeap(int maxSize, Comparator<T> comparator) {
+    super(maxSize, comparator);
+  }
+
+  void heapify(int curr) {
+    // if not leaf and greater
+    if (!isLeaf(curr)
+            && (greaterThan(array[curr], array[leftChild(curr)])) // greater than left child
+        || greaterThan(array[curr], array[rightChild(curr)])) { // greater than right child
+      if (!greaterThan(array[leftChild(curr)], array[rightChild(curr)])) {
+        swap(curr, leftChild(curr));
+        heapify(leftChild(curr));
+      } else {
+        swap(curr, rightChild(curr));/**/
+        heapify(rightChild(curr));
+      }
+    }
+  }
+}
