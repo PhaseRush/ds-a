dd90c224602a57fb79e8451b0a722644e86e8d62
phaserush <leozhang888@yahoo.ca>
2020-04-07 15:09:02 -0700

    day 7 one liner

--- src/main/leetcode/advent/week1/Day7.java
+++ src/main/leetcode/advent/week1/Day7.java
@@ -1,13 +1,26 @@
 package main.leetcode.advent.week1;
 
+import kotlin.Pair;
+
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Day7 {
     public static void main(String[] args) {
-        int[] in = {1,2,3};
-        System.out.println(countElements(in));
+        int[] in = {1, 2, 3};
+        System.out.println(oneLiner(in));
+    }
+
+    public static int oneLiner(int[] arr) {
+        return Arrays.stream(arr).boxed()
+                .map(i -> new Pair<>(i, Arrays.stream(arr).boxed().collect(Collectors.toSet())))
+                .map(pair -> {
+                    if (pair.getSecond().contains(pair.getFirst() + 1)) return 1;
+                    else return 0;
+                })
+                .reduce(Integer::sum)
+                .orElseThrow();
     }
 
     public static int countElements(int[] arr) {
