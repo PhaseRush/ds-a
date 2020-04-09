1dfc3334bc801743f104c882d6f8b5c44d98b386
phaserush <leozhang888@yahoo.ca>
2020-04-08 18:52:20 -0700

    note to listnode

--- src/main/leetcode/structure/ListNode.java
+++ src/main/leetcode/structure/ListNode.java
@@ -1,5 +1,9 @@
 package main.leetcode.structure;
 
+/**
+ * Node structure from Leetcode.
+ * Exists in this repo to make importing and speed running leetcode easier
+ */
 public class ListNode {
     public int val;
     public ListNode next;
