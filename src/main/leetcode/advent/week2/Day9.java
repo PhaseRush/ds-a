18d44f231a6ded74a35ab6554e61ac1526d7f35b
phaserush <leozhang888@yahoo.ca>
2020-04-10 00:32:36 -0700

    day 9 refactor

--- src/main/leetcode/advent/week2/Day9.java
+++ src/main/leetcode/advent/week2/Day9.java
@@ -7,23 +7,21 @@ public class Day9 {
         System.out.println(backspaceCompare("a##c", "#a#c"));
     }
 
+    private static Stack<Character> f(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '#') {
+                if (!stack.isEmpty()) stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack;
+    }
+
     public static boolean backspaceCompare(String S, String T) {
-        Stack<Character> stackS = new Stack<>();
-        for (char c : S.toCharArray()) {
-            if (c == '#') {
-                if (!stackS.isEmpty()) stackS.pop();
-            } else {
-                stackS.push(c);
-            }
-        }
-        Stack<Character> stackT = new Stack<>();
-        for (char c : T.toCharArray()) {
-            if (c == '#') {
-                if (!stackT.isEmpty()) stackT.pop();
-            } else {
-                stackT.push(c);
-            }
-        }
+        Stack<Character> stackS = f(S);
+        Stack<Character> stackT = f(T);
         return stackS.equals(stackT);
     }
 }
