f63aa61a3f811de7be9f26abb3f940eecc7ffe32
phaserush <leozhang888@yahoo.ca>
2020-04-17 19:50:52 -0700

    day 17 cleanup

--- src/main/leetcode/advent/week3/Day17.java
+++ src/main/leetcode/advent/week3/Day17.java
@@ -1,12 +1,11 @@
 package main.leetcode.advent.week3;
 
-import main.datastructures.Pair;
-
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Day17 {
     public static void main(String[] args) {
-        System.out.println(numIslands(new char[][]{
+        System.out.println(numIslands2(new char[][]{
                 {'1', '1', '1', '1', '0'},
                 {'1', '1', '0', '1', '0'},
                 {'1', '1', '0', '0', '0'},
@@ -14,45 +13,39 @@ public class Day17 {
         }));
     }
 
+    private static final int[] DX = {1, 0, -1, 0};
+    private static final int[] DY = {0, 1, 0, -1};
 
-    private static void bfs(char[][] grid, Pair<Integer, Integer> start, Set<Pair<Integer, Integer>> dejaVu) {
-        Deque<Pair<Integer, Integer>> q = new ArrayDeque<>();
-        q.push(start);
-        while (!q.isEmpty()) {
-            Pair<Integer, Integer> curr = q.pop();
-            if (!dejaVu.contains(curr)) {
-                dejaVu.add(curr);
-                if (curr.first > 0 && grid[curr.first - 1][curr.second] == '1') {
-                    Pair<Integer, Integer> next = new Pair<>(curr.first - 1, curr.second);
-                    if (!dejaVu.contains(next)) q.push(next);
-                }
-                if (curr.first < grid.length - 1 && grid[curr.first + 1][curr.second] == '1') {
-                    q.push(new Pair<>(curr.first + 1, curr.second));
-                }
-                if (curr.second > 0 && grid[curr.first][curr.second - 1] == '1') {
-                    q.push(new Pair<>(curr.first, curr.second - 1));
-                }
-                if (curr.second < grid[0].length - 1 && grid[curr.first][curr.second + 1] == '1') {
-                    q.push(new Pair<>(curr.first, curr.second + 1));
+    private static void search(char[][] grid, int x, int y, boolean[][] visited) {
+        Deque<Integer> xs = new ArrayDeque<>();
+        Deque<Integer> ys = new ArrayDeque<>();
+        xs.push(x);
+        ys.push(y);
+        while (!xs.isEmpty()) {
+            int currX = xs.pop();
+            int currY = ys.pop();
+            if (currX >= 0 && currX < grid.length && currY >= 0 && currY < grid[0].length && grid[currX][currY] == '1' && !visited[currX][currY]) {
+                visited[currX][currY] = true;
+                for (int i = 0; i < DX.length; i++) {
+                    xs.push(currX + DX[i]);
+                    ys.push(currY + DY[i]);
                 }
             }
         }
     }
 
-    public static int numIslands(char[][] grid) {
-        Set<Pair<Integer, Integer>> dejaVu = new LinkedHashSet<>();
+    public static int numIslands2(char[][] grid) {
+        if (grid.length == 0) return 0;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         int islands = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                Pair<Integer, Integer> currPair = new Pair<>(i, j);
-                if (!dejaVu.contains(currPair) && grid[i][j] == '1') {
-                    bfs(grid, currPair, dejaVu);
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    search(grid, i, j, visited);
                     islands++;
                 }
             }
         }
         return islands;
     }
-
-
 }
