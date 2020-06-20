b8bc841d091b11113c49840aa320b8bdb523aa9e
phaserush <leozhang888@yahoo.ca>
2020-06-20 15:07:42 -0700

    cleaned up fact decl

--- src/main/misc/SubPalindromes.kt
+++ src/main/misc/SubPalindromes.kt
@@ -1,12 +1,10 @@
 package main.misc
 
-// todo: http://www.luschny.de/math/factorial/FastFactorialFunctions.htm
-private fun Int.factorial(): Long {
-    return (2..this.toLong()).reduce(Long::times)
-}
-
 // https://www.reddit.com/r/UBC/comments/hcacoz/has_anyone_interviewed_with_sap_before/fve1ez9?utm_source=share&utm_medium=web2x
 fun main() {
+    // todo: http://www.luschny.de/math/factorial/FastFactorialFunctions.htm
+    fun Int.factorial(): Long = (2..this.toLong()).reduce(Long::times)
+
     val input = readLine()!!
     val freqMap = input.groupingBy { it }.eachCount()
 
