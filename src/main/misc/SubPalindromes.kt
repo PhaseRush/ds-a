5a51ea019f5d07d17e8d32cc273ab6efd7a3ad5d
phaserush <leozhang888@yahoo.ca>
2020-06-20 15:02:08 -0700

    Added case for single/odd char appearances

--- src/main/misc/SubPalindromes.kt
+++ src/main/misc/SubPalindromes.kt
@@ -8,9 +8,15 @@ private fun Int.factorial(): Long {
 // https://www.reddit.com/r/UBC/comments/hcacoz/has_anyone_interviewed_with_sap_before/fve1ez9?utm_source=share&utm_medium=web2x
 fun main() {
     val input = readLine()!!
-    val evenFreqMap = input.groupingBy { it }.eachCount()
-            .filterValues { freq -> freq % 2 == 0 }
+    val freqMap = input.groupingBy { it }.eachCount()
 
-    println("At least one palindrome: " + evenFreqMap.any())
-    println("Number of possible palindromes: " + evenFreqMap.count().factorial())
+    println("At least one palindrome: " +
+            freqMap.filterValues { freq -> freq % 2 == 0 }.any())
+
+    println("Number of possible palindromes: " +
+            freqMap.filterValues { freq -> freq % 2 == 0 }
+                    .count()
+                    .factorial()
+                    .times(freqMap.filterValues { freq -> freq % 2 == 1 }
+                            .count() + 1))
 }
